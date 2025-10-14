a24c5b3e620d07a334da7a96599f84ee6aa4957d
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2025-10-14 22:36:49 +0100

    implement routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
 
-const fetchData = 33
+const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/menu", element: <Menu /> },
+    { path: "/cart", element: <Cart /> },
+    { path: "/order/new", element: <CreateOrder /> },
+    { path: "/order/:orderId", element: <Order /> },
+]);
 
 function App() {
-  return (
-    <div>App</div>
-  )
+    return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
